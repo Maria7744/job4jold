0269ee6586424917f2c0a0e35fbf5816cc4d693c
pola7744@mail.ru <pola7744@mail.ru>
2020-01-10 20:13:05 +0300

    Интеграция Git Idea #212912

---
+++ src/main/java/job4j/loop/Counter.java
@@ -0,0 +1,20 @@
+package job4j.loop;
+
+public class Counter {
+    public int add(int start, int finish) {
+        int sum = 0;
+        finish = 10;
+
+
+        for (start = 2; start <= finish; start++) {
+
+            while (start % 2 == 0) {
+                sum += start;
+
+            return sum;
+        }
+    }
+
+
+
+
