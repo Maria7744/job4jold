55f91a356108d96a3b29d465b6cac87cdffe5e81
Maria7744 <pola7744@mail.ru>
2020-01-23 22:41:00 +0300

    223217 Реализовать Стэк (исправленный без наследования)

---
+++ src/ru/list/SimpleStack.java
@@ -0,0 +1,37 @@
+package src.ru.list;
+
+
+import java.util.EmptyStackException;
+
+public class SimpleStack<T> {
+    /**
+     * List container.
+     */
+    public ListContainer<T> list = new ListContainer<>();
+
+    /**
+     * Stack poll removes and return element added most recently.
+     *
+     * @return Element added most recently.
+     */
+    public T poll() {
+        T element = null;
+        try {
+            element = list.remove(list.size() - 1);
+        } catch (IndexOutOfBoundsException ie) {
+            throw new EmptyStackException();
+        }
+        return element;
+    }
+
+    /**
+     * Stack push adds element.
+     *
+     * @param value Element to add.
+     */
+    public void push(T value) {
+        this.list.add(value);
+    }
+}
+
+
