55b98815cbfd8cab3ab3d67657f6024c04a5a5e2
Maria7744 <pola7744@mail.ru>
2020-02-04 23:53:55 +0300

    223218 создать очередь на 2 стеках

--- chapter_001/src/main/java/list/ArrayStack.java
+++ chapter_001/src/main/java/list/ArrayStack.java
@@ -3,39 +3,46 @@ package list;
 
 import java.util.Stack;
 
-public class ArrayStack <T>extends Stack {
-        private final int capacity;
-        private int headPointer;
-        private final int[] data;
-        public ArrayStack(int capacity) {
-            this.capacity = capacity;
-            this.data = new int[capacity];
-        }
-        public void push(int a) {
-            data[headPointer] = a;
-            headPointer = (headPointer + 1) % capacity;
-        }
-        public Object peek() {
-            return data[headPointer];
+public class ArrayStack<T> extends Stack {
+    private final int capacity;
+    private int headPointer;
+    private final int[] data;
 
-        }
-        public Object pop() {
-            headPointer = (headPointer + 1) % capacity;
-            return null;
-        }
-        public int getSize() {
-            int size = (headPointer +1) % capacity;
-            if (size < 0) {
-                size += capacity;
-            }
-            return size;
-        }
-        public boolean isEmpty() {
-            return getSize() == 0;
-        }
-        public boolean isFull() {
-            return getSize() == capacity;
-        }
+    public ArrayStack(int capacity) {
+        this.capacity = capacity;
+        this.data = new int[capacity];
+    }
+
+    public void push(int a) {
+        data[headPointer] = a;
+        headPointer = (headPointer + 1) % capacity;
+    }
+
+    public Object peek() {
+        return data[headPointer];
 
     }
+
+    public Object pop() {
+        headPointer = (headPointer + 1) % capacity;
+        return null;
+    }
+
+    public int getSize() {
+        int size = (headPointer + 1) % capacity;
+        if (size < 0) {
+            size += capacity;
+        }
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+
+    public boolean isFull() {
+        return getSize() == capacity;
+    }
+
+}
 
--- chapter_001/src/main/java/list/SimpleQuire.java
+++ chapter_001/src/main/java/list/SimpleQuire.java
@@ -4,50 +4,52 @@ import java.util.Queue;
 import java.util.Stack;
 
 public abstract class SimpleQuire implements Queue {
-  
-           
-   
-           
-          
-        Stack s1= new ArrayStack(4);
-        Stack s2= new ArrayStack(4);
-        public void enqueue(int a) {
-            s1.push(a);
-        }
+
+
+    Stack s1 = new ArrayStack(4);
+    Stack s2 = new ArrayStack(4);
+
+    public void enqueue(int a) {
+        s1.push(a);
+    }
+
     /*peek – позволяет получить значение элемента, находящегося в
     начале очереди.*/
-        public Object peek() {
-            if(s2.isEmpty()!=true)
-                s2.peek();
-            else
-                while(s1.isEmpty() == false){
-                    s2.push(s1.peek());
-                    s1.pop();
-                }
-            return  s2.peek();
-        }
-        /* dequeue – удаляет элемент, находящийся в начале очереди.*/
-        public void dequeue() {
-            if (s2.isEmpty()!=true)
-                s2.pop();
-            else
-                while(s1.isEmpty()==false){
-                    s2.push(s1.peek());
-                    s1.pop();
-                }
+    public Object peek() {
+        if (s2.isEmpty() != true)
+            s2.peek();
+        else
+            while (s1.isEmpty() == false) {
+                s2.push(s1.peek());
+                s1.pop();
+            }
+        return s2.peek();
+    }
+
+    /* dequeue – удаляет элемент, находящийся в начале очереди.*/
+    public void dequeue() {
+        if (s2.isEmpty() != true)
             s2.pop();
-
-        }
-        /* getSize – возвращает количество элементов, находящихся в
-    очереди.*/
-        public int getSize() {
-            return s1.size()+s2.size();
-        }
-   /* isEmpty – возвращает true, если в очереди нет ни одного элемента.*/
-        public boolean isEmpty(){
-            return getSize()==0;
-        }
-
+        else
+            while (s1.isEmpty() == false) {
+                s2.push(s1.peek());
+                s1.pop();
+            }
+        s2.pop();
 
     }
+
+    /* getSize – возвращает количество элементов, находящихся в
+очереди.*/
+    public int getSize() {
+        return s1.size() + s2.size();
+    }
+
+    /* isEmpty – возвращает true, если в очереди нет ни одного элемента.*/
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+
+
+}
 
