3035801535ba88687f3570ef8a9d850e7a5c867a
Maria7744 <pola7744@mail.ru>
2020-02-18 20:02:38 +0300

    исправленный итерратор для двухмерного массива

---
+++ chapter_001/src/main/java/iterator/MatrixIteratorN.java
@@ -0,0 +1,57 @@
+package iterator;
+
+
+import java.util.Iterator;
+
+public class  MatrixIteratorN <T>implements Iterable<T>  {
+
+        private T[][] values;
+
+        private int row;
+
+        private int col;
+
+        public MatrixIteratorN(T[][] values) {
+
+            this.values = values;
+
+        }
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<T>() {
+
+            @Override
+
+            public boolean hasNext() {
+
+                return row < values.length && col < values[row].length;
+
+            }
+
+            @Override
+
+            public T next() {
+                T value;
+                if (this.hasNext()) {
+
+                    value = values[row][col++];
+
+                    if (col <= values[row].length) {
+
+                        row++;
+
+                        col = 0;
+
+                    }
+                }
+                return values[row][col++];
+
+            }
+
+        };
+    }
+}
+
+
+
