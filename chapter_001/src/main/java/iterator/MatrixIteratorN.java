82b70ffd6c54b4f63bf14d38675e11af5c530c3b
Maria7744 <pola7744@mail.ru>
2020-02-21 20:33:19 +0300

    исправленный пробелы итерратор для двухмерного массива

--- chapter_001/src/main/java/iterator/MatrixIteratorN.java
+++ chapter_001/src/main/java/iterator/MatrixIteratorN.java
@@ -3,52 +3,38 @@ package iterator;
 
 import java.util.Iterator;
 
-public class  MatrixIteratorN <T>implements Iterable<T>  {
+public class MatrixIteratorN<T> implements Iterable<T> {
 
-        private T[][] values;
+    private T[][] values;
+    private int row;
+    private int col;
 
-        private int row;
+    public MatrixIteratorN(T[][] values) {
+        this.values = values;
+    }
 
-        private int col;
-
-        public MatrixIteratorN(T[][] values) {
-
-            this.values = values;
-
-        }
     @Override
     public Iterator<T> iterator() {
 
         return new Iterator<T>() {
 
             @Override
-
             public boolean hasNext() {
-
                 return row < values.length && col < values[row].length;
-
             }
 
             @Override
-
             public T next() {
                 T value;
                 if (this.hasNext()) {
-
                     value = values[row][col++];
-
                     if (col <= values[row].length) {
-
                         row++;
-
                         col = 0;
-
                     }
                 }
                 return values[row][col++];
-
             }
-
         };
     }
 }
