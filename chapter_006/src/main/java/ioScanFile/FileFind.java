8fc09905fe9a8f736f11723e47599c5bb88487e1
Maria7744 <pola7744@mail.ru>
2020-04-07 10:01:05 +0300

    223252 сканипрование файловой системы с помощью Path

---
+++ chapter_006/src/main/java/ioScanFile/FileFind.java
@@ -0,0 +1,29 @@
+package ioScanFile;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FileFind {
+
+    public static void main(String[] args) throws IOException {
+        Path directory = Paths.get(".");
+        String fileNameToFind = ".gs";
+
+        try {
+
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes fileAttributes) {
+                    if (path.toFile().getName().equals(fileNameToFind)) {
+                        System.out.println("Found: " + path.toString());
+                        return FileVisitResult.TERMINATE;
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
